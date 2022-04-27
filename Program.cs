006d1a4e992d6d9ec8280e5e66e14f9b621d941f
hugov2685 <hugov2685@yahoo.com>
2022-04-27 15:12:58 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRUEBA_MSTEST
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(createMessage());
+        }
+        public static String createMessage ()
+        {
+            return "Hola Mundo";
+        }
+    }
+}
